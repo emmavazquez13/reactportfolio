28b833cc7d4663c1de236d3f9a9bba3ab84dd380
emma Vazquez <emmavazquez700@gmail.com>
2023-01-28 18:20:10 -0600

    header.js

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+<Header>
+    <a href="/" className='logo text-2xl font-bold'>Emma Vazquez</a>
+
+    {/* Desktop Nav */}
+    <ul className='flex'>
+        <li><a href="/#">About</a></li>
+        <li><a href="/#projects">Projects</a></li>
+        <li><a href="/#blog">Blog</a></li>
+        <li><a href="/#contact">Contact</a></li>
+        <li><a href="/#resume">Resume</a></li>
+
+
+    </ul>
+    {/* Mobile Nav */}
+
+    {/* Toggle Button */}
+</Header>
+  )
+}
+
+export default Header
