6a0ba481c716fc725145162e5e7b2e22ff7cb930
emma Vazquez <emmavazquez700@gmail.com>
2023-01-28 20:03:39 -0600

    styles

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -2,23 +2,26 @@ import React from 'react'
 
 const Header = () => {
   return (
-<Header>
+<header className='flex justify-between px-5 py-2 bg-gray-800 text-white'>
     <a href="/" className='logo text-2xl font-bold'>Emma Vazquez</a>
 
+    <nav>
     {/* Desktop Nav */}
-    <ul className='flex'>
+    <ul className="flex">
         <li><a href="/#">About</a></li>
         <li><a href="/#projects">Projects</a></li>
         <li><a href="/#blog">Blog</a></li>
         <li><a href="/#contact">Contact</a></li>
         <li><a href="/#resume">Resume</a></li>
-
-
     </ul>
+
     {/* Mobile Nav */}
 
     {/* Toggle Button */}
-</Header>
+
+    </nav>
+
+</header>
   )
 }
 
