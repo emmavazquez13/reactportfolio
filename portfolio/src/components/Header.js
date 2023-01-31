7eb459125d2a485868f320c4b85b7c45a65e52d9
emma Vazquez <emmavazquez700@gmail.com>
2023-01-30 23:18:24 -0600

    header

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,28 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => setToggle(!toggle);
+
   return (
-<header className='flex justify-between px-5 py-2 bg-gray-800 text-white'>
-    <a href="/" className='logo text-2xl font-bold'>Emma Vazquez</a>
+    <header className="flex justify-between px-5 py-2 bg-primary text-white fixed w-full z-10">
+      <a href="/" className="logo text-2xl font-bold text-accent">
+        Emma Vazquez
+      </a>
 
-    <nav>
-    {/* Desktop Nav */}
-    <ul className="flex">
-        <li><a href="/#">About</a></li>
-        <li><a href="/#projects">Projects</a></li>
-        <li><a href="/#blog">Blog</a></li>
-        <li><a href="/#contact">Contact</a></li>
-        <li><a href="/#resume">Resume</a></li>
-    </ul>
+      {/* Desktop Nav */}
+      <nav className="hidden md:block">
+        <ul className="flex">
+          <li>
+            <a href="/#about">About</a>
+          </li>
+          <li>
+            <a href="/#projects">Projects</a>
+          </li>
+          <li>
+            <a href="/#blog">Blog</a>
+          </li>
+          <li>
+            <a href="/#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#resume -link" target="_blank" without rel="noreferrer">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
 
-    {/* Mobile Nav */}
+      {/* Mobile Nav */}
+      <nav
+        className={!toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}
+      >
+        <ul className="flex flex-col">
+          <li>
+            <a href="/#about">About</a>
+          </li>
+          <li>
+            <a href="/#projects">Projects</a>
+          </li>
+          <li>
+            <a href="/#blog">Blog</a>
+          </li>
+          <li>
+            <a href="/#contact">Contact</a>
+          </li>
+          <li>
+            <a href="/#resume">Resume</a>
+          </li>
+        </ul>
+      </nav>
 
-    {/* Toggle Button */}
+      {/* Toggle button */}
+      <button onClick={handleToggle} className="block md:hidden">
+        {!toggle ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
+      </button>
+    </header>
+  );
+};
 
-    </nav>
-
-</header>
-  )
-}
-
-export default Header
+export default Header;
